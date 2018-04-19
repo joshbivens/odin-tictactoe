38bf4b1e376af42b7fb70e5848ce35b532f5f0a0
Josh Bivens <hotbioform@gmail.com>
2018-04-19 12:28:31 -0600

    Start js

---
+++ game.js
@@ -0,0 +1,16 @@
+function GameBoard() {
+  let gameboard = ['', '', '', '', '', '', '', '', ''];
+  return {
+    gameboard
+  }
+}
+
+function Player(side) {
+  return {
+    side
+  }
+}
+
+function Game() {
+  const newGame = GameBoard();
+}
