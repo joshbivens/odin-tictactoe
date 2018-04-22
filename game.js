ad5f83b1bd4d1b7ea2d37c4912ff86e470366760
Josh Bivens <hotbioform@gmail.com>
2018-04-22 06:00:38 -0600

    Commit for push

--- game.js
+++ game.js
@@ -14,36 +14,41 @@ const gameBoard = (() => {
 
 // Gameplay needs to house movement and winstate
 const gamePlay = (() => {
+
   // const checkWin = () => {
 
   // }
 
-  const makeMove = (e) => {
+  const markSpace = (e, side) => {
+    game.newGame.gameboard[e.target.dataset.id] = side;
+  }
+
+  const initiateMove = (e) => {
     game.switchSides = !game.switchSides;
     if (game.switchSides) {
-      game.newGame.gameboard[e.target.dataset.id] = "O";
+      markSpace(e, "O");
       render(game.player1);
     } else {
-      game.newGame.gameboard[e.target.dataset.id] = "X";
+      markSpace(e, "X");
       render(game.player2);
     }
     // checkWin();
   }
 
   return {
-    makeMove
+    initiateMove
   }
 })();
 // Init needs to initialize the players and gameboard
 const game = (() => {
-  const player1 = Player("Josh");
-  const player2 = Player("Chris");
+  const player1 = "Josh"; // prompt("Enter the first player's name.");
+  const player2 = "Chris"; // prompt("Enter the second player's name.");
   const info = document.querySelector(".info");
   const grid = document.querySelectorAll(".space");
   let newGame = gameBoard;
   let switchSides = true;
 
-  grid.forEach(space => space.addEventListener("click", gamePlay.makeMove));
+  grid.forEach(space => space.addEventListener("click", gamePlay.initiateMove));
 
   return {
     switchSides,
@@ -51,18 +56,38 @@ const game = (() => {
     player1,
     player2,
     grid,
-    info
+    info,
   }
-})(); 
+})();
 
 // Render needs to render the everything after a move is made
 // return player from makeMove and use it in render?
 const render = (player) => {
   game.grid.forEach((space, index) => space.innerHTML = game.newGame.gameboard[index]);
-  game.info.innerHTML = `${player.name}'s turn`;
+  if (!firstTurn) {
+    game.info.innerHTML =
+      `<div>
+        ${player.name}'s turn
+      </div>`;
+  }
 };
 
+let firstTurn = true;
+
+const firstTurnFunction = () => {
+  console.log(game.player1.name)
+  firstTurn = false;
+  game.info.innerHTML =
+    `<div>
+      <div>${game.player1.name} is X, ${game.player2.name} is O.</div>
+    </div>`;
+}
+
 game;
-render();
+console.log(game.player1)
+setTimeout(() => {
+  render();
+  firstTurnFunction();
+}, 500)
 
 // console.log(`${player1.name} (${player1.side}) vs ${player2.name} (${player2.side})!`);
